e88a568a1943d70c364ed0d7d80170e1540fb0db
Susan <susan.elaine.maas@gmail.com>
2019-09-16 12:55:40 +0200

    Add first draft of legal information.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,10 @@
 import React from "react"
 import footerStyles from "./footer.module.css"
+import { Link } from "gatsby"
 
 export default () => (
     <div className={footerStyles.footer}>
         <small>Copyright 2019, Susan Maas</small>
+        <small><Link to="legal">Legal</Link></small>
     </div>
 )
---
+++ src/pages/legal.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>Legal Information</h1>
+        <h2>Site Owner</h2>
+        <p>This site is owned and maintained by Susan Maas</p>
+        <h2>Site Hosting</h2>
+        <p>Netlify, 2325 3rd St #215, San Francisco, CA, United States</p>
+        <p>Telephone: 844-899-7312</p>
+    </Layout>
+)
