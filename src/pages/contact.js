acb236b7d6ac3114773207e94deb32be792661ed
Susan <susan.elaine.maas@gmail.com>
2019-09-16 13:02:16 +0200

    Modify contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 
 export default () => (
     <Layout>
-        <h2>Coming Soon!</h2>
-        <p>This page is still under construction. I'll get it up and running as soon as possible.</p>
+        <h2>Contact Form Coming Soon!</h2>
+        <p>I'm working on setting up a contact form. I'll get it up and running as soon as possible. Until then, you can reach out to me on Twitter: @SusanElaineMaas</p>
     </Layout>
 )
