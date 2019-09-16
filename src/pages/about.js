5e45ed6b82a4ac0e3e81cdb15d63669a454c7ea1
Susan <susan.elaine.maas@gmail.com>
2019-09-16 10:59:59 +0200

    Modify responsiveness of logo at 575px. Modify About page text.

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ export default () => (
     <Layout>
         <h1>About this Blog</h1>
         <p>Hi, I'm Susan! I'm teaching myself web development.</p>
-        <p>My goal is to provide you with informative, helpful posts to help you get started with your learning.</p>
+        <p>My goal is to provide you with informative, useful posts to help you get started with your learning.</p>
         <p>So jump in, and let's do this together!</p>
         <p>If you'd like to reach me, you can use my <Link className="blueLinks" to="/contact/">contact page</Link>.</p>
         <p>Happy reading!</p>
